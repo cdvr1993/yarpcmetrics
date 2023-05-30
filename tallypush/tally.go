3fed560646151b5ee3daf37167ba02dcfeb0fc18
Cristian Velazquez <cdvr@uber.com>
2023-05-30 23:46:01 +0000

    Add infinity bucket

--- tallypush/tally.go
+++ tallypush/tally.go
@@ -23,7 +23,6 @@
 package tallypush // import "go.uber.org/net/metrics/tallypush"
 
 import (
-	"fmt"
 	"math"
 
 	"github.com/uber-go/tally"
@@ -60,14 +59,15 @@ func (tp *target) NewHistogram(spec push.HistogramSpec) push.Histogram {
 			buckets[i] = float64(spec.Buckets[i])
 		}
 	}
-	tallyBuck := tally.ValueBuckets(buckets)
+	internalBuckets := append(spec.Buckets, math.MaxInt64)
 	return &histogram{
 		Histogram: tp.Tagged(spec.Tags).Histogram(
 			spec.Name,
-			tallyBuck,
+			tally.ValueBuckets(buckets),
 		),
-		buckets: tallyBuck,
-		lasts:   make([]int64, len(spec.Buckets)),
+		buckets: internalBuckets,
+		// always add infinity
+		lasts: make([]int64, len(internalBuckets)),
 	}
 }
 
@@ -97,16 +97,15 @@ type histogram struct {
 	// lasts keep the last value pushed to tally per histogram bucket.  This
 	// defaults to zero.
 	//lasts map[int64]int64
-	buckets tally.ValueBuckets
+	buckets []int64
 	lasts   []int64
 }
 
 func (th *histogram) Set(bucketIndex int64, total int64) {
-	fmt.Println("LABEL 2:", bucketIndex, len(th.lasts))
 	delta := total - th.lasts[bucketIndex]
 	th.lasts[bucketIndex] = total
 
 	for i := int64(0); i < delta; i++ {
-		th.RecordValue(th.buckets[bucketIndex])
+		th.RecordValue(float64(th.buckets[bucketIndex]))
 	}
 }
